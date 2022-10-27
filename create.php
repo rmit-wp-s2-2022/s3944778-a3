74c63c4dafd6a43ab4f6d5e8a7dff4179513d70c
Michaelk003 <101307754+Michaelk003@users.noreply.github.com>
2022-10-27 22:37:57 +1100

    task 3 (check extended description)
    task 3 without proper css styling and sitemap
    include .htaccessfile

---
+++ create.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>s3944778 - a3</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Michael">
+        <meta name="description" content="home page">
+
+        <link rel="stylesheet" type="text/css" href="a3.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>  
+        <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+        <noscript>This web browser does not support Javascript</noscript>
+    </head>
+
+    <body> 
+    <h1>create</h1>
+    </body>
+
+    <footer>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="create.php">Create</a></li>
+        </ul>
+    </footer>
+
+</html>
